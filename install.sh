3ebbb91e4cc5f77232427785ba6ada5aa45b3539
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-25 21:24:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,4 +38,14 @@ curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
 echo "$SOURCE_REPO" | sudo tee /etc/apt/sources.list.d/raspotify.list
 
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get -y install raspotify
+# Script for Airplay
+sudo apt-get -y install uild-essential git xmltoman autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev
+cd shairport-sync
+autoreconf -fi
+./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd
+make
+sudo make install
+systemctl enable shairport-sync
+systemctl start shairport-sync
