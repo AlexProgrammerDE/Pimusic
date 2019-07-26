d9fa1a759d0b00b88c0c0346cfd6e04a150e5ec9
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-26 09:42:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,46 +1,47 @@
 #!/bin/sh
-echo "Pi-Soundplayer"
-# Get updates
-echo "Updating System"
-sudo apt-get update
-sudo apt-get upgrade
-# Install needed packages
-echo "Getting needed Packages"
-sudo apt-get -y install gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-gst-1.0 build-essential python-dev python-pip curl alsa-base alsa-utils bluealsa bluez bluez-firmware python-gobject python-dbus mpg123apt-transport-https uild-essential git xmltoman autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev
-
-# Add repo and its GPG key
-curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
-echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
-
-# Install package
-echo "Installing Spotify Support"
-sudo apt-get -y install raspotify
-
-# Script for Airplay
-echo "Installing Airplay Support"
-git clone https://github.com/mikebrady/shairport-sync.git
-cd shairport-sync
-
-autoreconf -fi
-./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd
-make
-sudo make install
-systemctl enable shairport-sync
-systemctl start shairport-sync
-
-#Mopidy
-echo "Installing Mopidy Support"
-sudo pip install -U mopidy
-
-# Bluetooth script
-echo "Installing Bluetooth Support"
-git clone https://github.com/AdityaTelange/rpi-bluetooth-audio-player
-cd rpi-bluetooth-audio-player
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
-else
+	else
+	echo "Pi-Soundplayer"
+	# Get updates
+	echo "Updating System"
+	sudo apt-get update
+	sudo apt-get upgrade
+	# Install needed packages
+	echo "Getting needed Packages"
+	sudo apt-get -y install gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-gst-1.0 build-essential python-dev python-pip curl alsa-base alsa-utils bluealsa bluez bluez-firmware python-gobject python-dbus mpg123apt-transport-https uild-essential git xmltoman autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev
+
+	# Add repo and its GPG key
+	curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
+	echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
+
+	# Install package
+	echo "Installing Spotify Support"
+	sudo apt-get -y install raspotify
+
+	# Script for Airplay
+	echo "Installing Airplay Support"
+	git clone https://github.com/mikebrady/shairport-sync.git
+	cd shairport-sync
+
+	autoreconf -fi
+	./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd
+	make
+	sudo make install
+	systemctl enable shairport-sync
+	systemctl start shairport-sync
+
+	#Mopidy is in planning
+	#echo "Installing Mopidy Support"
+	#sudo pip install -U mopidy
+
+	# Bluetooth script
+	echo "Installing Bluetooth Support"
+	git clone https://github.com/AdityaTelange/rpi-bluetooth-audio-player
+	cd rpi-bluetooth-audio-player
+
 	echo "installing Bluetooth Music Player"
 	export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 	export UDEV=1
