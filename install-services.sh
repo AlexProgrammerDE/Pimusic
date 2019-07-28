7160e217da8f87759a16e0299b79dbc8277acbc6
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-28 10:54:27 +0200

    Update install-services.sh

--- install-services.sh
+++ install-services.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 	echo "Pimusic Installation"
 	echo ""
 	echo "Preparing the System for Installation"
-	echo""
+	echo ""
 	echo "Adding needed Keys."
 	# Spotify Key
 	curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
@@ -20,26 +20,35 @@ if [[ $EUID -ne 0 ]]; then
 	
 	# Get updates
 	echo "Updating System"
+	echo ""
 	sudo apt-get update
 	sudo apt-get -y upgrade
 	sudo apt -y autoremove
 	# Install needed packages 
 	echo "Getting needed Packages"
+	echo ""
 	sudo apt-get -y install wget xdg-open gstreamer1.0-plugins-bad gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-gst-1.0 python-dev python-pip curl alsa-base alsa-utils bluealsa bluez bluez-firmware python-gobject python-dbus mpg123 autotools-dev apt-transport-https dh-autoreconf git xmltoman autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev
 
 	# Making directory for data.
 	mkdir service_data
 	cd service_data
+	echo ""
 	echo "Installing"
+	echo ""
 	# Install package
 	echo "Installing Spotify Support"
+	echo ""
 	sudo apt-get -y install raspotify
 
 	# Script for Airplay
+	echo ""
 	echo "Installing Airplay Support"
+	echo ""
 	git clone https://github.com/mikebrady/shairport-sync.git
 	cd shairport-sync
+	echo ""
 	echo "Wait please. This can take some time."
+	echo ""
 	autoreconf -fi
 	./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd
 	make
@@ -48,7 +57,9 @@ if [[ $EUID -ne 0 ]]; then
 	systemctl start shairport-sync
 
 	# Mopidy script 
+	echo ""
 	echo "Installing Mopidy Support"
+	echo ""
 	sudo apt-get -y install mopidy
 	sudo systemctl enable mopidy
 	sudo systemctl start mopidy
@@ -65,14 +76,18 @@ if [[ $EUID -ne 0 ]]; then
 	pip install Mopidy-TuneIn
 	
 	# Bluetooth script
+	echo ""
 	echo "Installing Bluetooth Support"
+	echo ""
 	cd ..
 	git clone https://github.com/AlexProgrammerDE/rpi-bluetooth-audio-player.git
 	cd rpi-bluetooth-audio-player
 	export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
 	# Copy sounds
+	echo ""
 	echo "copying files"
+	echo ""
 	cp -r ./bluetooth-player/sounds/ /usr/src/sounds/
 
 	# Setup udev rules - this lets us play the connect/disconnect sound
@@ -87,13 +102,17 @@ if [[ $EUID -ne 0 ]]; then
 
 	cp -r ./bluetooth-player/start.sh /usr/src/
 	chmod +x /usr/src/start.sh
-
+	
+	echo ""
 	echo "installing bluetooth-player service"
+	echo ""
 	sudo ln -sf /usr/src/start.sh /usr/local/bin/start_bt_player
 	cp ./bluetooth-player/bluetooth-player.service /etc/systemd/system/
-
+	echo""
 	echo "done !!!" 
+	echo ""
 	echo "Starting Bluetooth Support"
+	echo ""
 	systemctl start bluetooth-player.service
 
 fi
