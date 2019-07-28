2daef2e8ef165ce88b0a4a4bddd46644c1b838d9
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-28 07:57:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" | tee -a output.txt
    exit 1
 	else
-    mkdir Musicpi
-    cd Musicpi
-    wget https://raw.githubusercontent.com/AlexProgrammerDE/Musicpi/master/install-services.sh
-    sudo bash install-services.sh | tee -a output.txt
-    cd ..
-    rm install.sh
+    	mkdir Musicpi
+    	cd Musicpi
+    	wget https://raw.githubusercontent.com/AlexProgrammerDE/Musicpi/master/install-services.sh
+    	sudo bash install-services.sh | tee -a output.txt
+    	cd ..
+    	rm install.sh
 fi
