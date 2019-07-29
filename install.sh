fb44b92fecab338781d040d66476a85c11eaa0e2
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-29 10:26:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" | tee -a output.txt
+   echo "This script must be run as root"
    exit 1
 	else
     	mkdir Pimusic
     	cd Pimusic
     	wget https://raw.githubusercontent.com/AlexProgrammerDE/Pimusic/master/install-services.sh
-    	sudo bash install-services.sh | tee -a output.txt
+    	sudo bash install-services.sh | tee -a install-output.txt
 	rm install-services.sh
 	wget https://raw.githubusercontent.com/AlexProgrammerDE/Pimusic/master/update-services.sh
     	cd ..
