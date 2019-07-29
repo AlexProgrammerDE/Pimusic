fe11590f6f56e25038742c6ff0c12be8968adb87
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-07-29 09:38:44 +0200

    Update update-services.sh

--- update-services.sh
+++ update-services.sh
@@ -4,10 +4,22 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 	else
-  clear
-  echo "Pimusic update"
+  	clear
+  	echo "Pimusic update"
+	sudo systemctl stop shairport-sync
+	echo "removing bletooth support files"
 
+	rm -rf /usr/src/sounds/
 
+	rm -rf /usr/src/bluetooth-udev
+	rm -rf /etc/udev/rules.d/udev-rules/
+	rm -rf /usr/src/bluetooth-agent
+	rm -rf /usr/src/start.sh
+	rm -rf /usr/local/bin/start_bt_player
+
+	systemctl stop bluetooth-player.service
+	systemctl disable bluetooth-player.service
+	rm -rf /etc/systemd/system/bluetooth-player.service
 
 
 fi
