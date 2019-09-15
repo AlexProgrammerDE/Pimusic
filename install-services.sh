d798c01f5d4f63c1a10c72aacb7101d10cdaa252
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-09-15 13:27:18 +0200

    Update install-services.sh

--- install-services.sh
+++ install-services.sh
@@ -89,36 +89,6 @@ if [[ $EUID -ne 0 ]]; then
 	cd ..
 	git clone https://github.com/AlexProgrammerDE/rpi-bluetooth-audio-player.git
 	cd rpi-bluetooth-audio-player
-	export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-	export UDEV=1
-
-	echo "installing packages.."
-	sudo apt-get install alsa-base alsa-utils bluealsa bluez bluez-firmware python-gobject python-dbus mpg123
-	# bluez-firmware
-
-	# Copy sounds
-	echo "copying files"
-	cp -r ./bluetooth-player/sounds/ /usr/src/sounds/
-
-	# Setup udev rules - this lets us play the connect/disconnect sound
-	# and also turn off discover/pairing when a client is connected
-	cp -r ./bluetooth-player/bluetooth-udev /usr/src/
-	chmod +x /usr/src/bluetooth-udev
-	cp -r ./bluetooth-player/udev-rules/ /etc/udev/rules.d/
-
-	# Bluetooth-agent handles the auth of devices
-	cp -r ./bluetooth-player/bluetooth-agent /usr/src/
-	chmod +x /usr/src/bluetooth-agent
-
-	cp -r ./bluetooth-player/start.sh /usr/src/
-	chmod +x /usr/src/start.sh
-
-	echo "installing bluetooth-player service"
-	sudo ln -sf /usr/src/start.sh /usr/local/bin/start_bt_player
-	cp ./bluetooth-player/bluetooth-player.service /etc/systemd/system/
-
-	echo "done !!!"
-	echo "starting bt player"
-	systemctl start bluetooth-player.service
+	sudo bash install.sh
 	clear
 fi
